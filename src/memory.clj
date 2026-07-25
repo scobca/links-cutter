4434e02f1f3668fb22362eb0f0de7f264521022b
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2026-07-26 02:10:34 +0300

    feat(memory): added memory reader

--- src/memory.clj
+++ src/memory.clj
@@ -12,3 +12,23 @@
 (defn remove-link-record!
   [code]
   (swap! links dissoc code))
+
+(defn show-links
+  ([]
+   (println "All links:")
+   (doseq [[code url] @links]
+     (println "  Code:" code "-> URL:" url)))
+
+  ([size]
+   (let [all-links @links
+         total (count all-links)
+         start (max 0 (- total size))
+         links-to-show (->> all-links
+                            (drop start)
+                            (take size))]
+
+     (println (str "Last " size " link(s) (total: " total "):"))
+     (if (empty? links-to-show)
+       (println "  No links to show")
+       (doseq [[code url] links-to-show]
+         (println "  Code:" code "-> URL:" url))))))
