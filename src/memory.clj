7ef5f579ce45d2e0d7fd0220ba2531f91e69e812
Vladimir Fokin <scobca18@yandex.ru>
2026-07-28 03:16:33 +0300

    docs(memory): added small docs for memory functions

--- src/memory.clj
+++ src/memory.clj
@@ -2,18 +2,23 @@
 
 (def links (atom {}))
 
-(defn code-occupied? [code]
+(defn code-occupied?
+  "Check for the presence of code into memory and return boolean.g"
+  [code]
   (contains? @links code))
 
 (defn save-link-record!
+  "Accepts short-code and natural link (url), save it into memory."
   [code url]
   (swap! links assoc code url))
 
 (defn remove-link-record!
+  "Accepts link's short-code and remove link record from memory if exists."
   [code]
   (swap! links dissoc code))
 
 (defn show-links
+  "Show all links records (if args are empty) or show last N records (if user provide param size)."
   ([]
    (println "All links:")
    (doseq [[code url] @links]
