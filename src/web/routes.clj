90a94f9553f8d93ff07db79bef58f38de7e53eca
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2026-08-03 04:08:39 +0300

    feat(routes): added /info & /show routes

--- src/web/routes.clj
+++ src/web/routes.clj
@@ -1,10 +1,42 @@
 (ns web.routes
-  (:require [compojure.api.sweet :refer :all]
-            [ring.util.http-response :refer :all]
-            [time :refer [get-server-uptime]])
+  (:require [cheshire.core :refer [generate-string]]
+            [compojure.core :refer [defroutes GET ANY]]
+            [ring.util.http-response :refer [ok not-found content-type]]
+
+            ;; Own code imports
+            [memory :refer [show-links]]
+            [time :refer [get-server-uptime]]
+            [constants :refer [BASIC-LINKS-PULL-SIZE APPLICATION-JSON]])
   (:import (time Uptime)))
 
-(def app-routes
-  (GET "/info" request
+(defroutes app-routes
+  (GET "/info" []
     (let [^Uptime uptime (get-server-uptime)]
-      (ok (str "Web server started successfully. Work time: " (:hours uptime) "h " (:minutes uptime) "min " (:seconds uptime) "sec " (:millis uptime) "millis")))))
+      (ok (str "Web server started successfully. Work time: "
+               (:hours uptime) "h "
+               (:minutes uptime) "min "
+               (:seconds uptime) "sec "
+               (:millis uptime) "millis"))))
+
+  (GET "/show/:length" request
+    (let [length (-> request :params :length parse-long)]
+      (cond
+        ;; if length = nil -> use default value
+        (nil? length) (-> (show-links BASIC-LINKS-PULL-SIZE)
+                          generate-string
+                          ok
+                          (content-type APPLICATION-JSON))
+
+        ;; if length = 0 -> show all links
+        (zero? length) (-> (show-links)
+                           generate-string
+                           ok
+                           (content-type APPLICATION-JSON))
+
+        ;; show last <length> links
+        :else (-> (show-links length)
+                  generate-string
+                  ok
+                  (content-type APPLICATION-JSON)))))
+
+  (ANY "*" [] (not-found "Not Found")))
