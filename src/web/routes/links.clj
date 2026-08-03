4ec147edd1b71d12789d29dba7f9ca683652e9a8
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2026-08-04 01:35:31 +0300

    refactor(api): added routes prefix

--- src/web/routes/links.clj
+++ src/web/routes/links.clj
@@ -1,5 +1,5 @@
 (ns web.routes.links
-  (:require [compojure.core :refer [defroutes GET POST DELETE]]
+  (:require [compojure.core :refer [defroutes context GET POST DELETE]]
             [ring.util.http-response :refer [not-found ok bad-request]]
             [core.memory :refer [show-links find-link-record remove-link-record!]]
             [domain.link :refer [create-link-record!]]
@@ -7,37 +7,38 @@
             [dto.link-request :refer [map->CreateLinkRequest]]))
 
 (defroutes links-routes
+  (context "/api/v1/links" []
 
-  (GET "/show/:length" request
-    "Shows links. Optional :length parameter for limiting results."
+    (GET "/:length" request
+      "Shows links. Optional :length parameter for limiting results."
 
-    (let [length (-> request :params :length parse-long)]
-      (cond
-        ;; if length = nil -> use default value
-        (nil? length) (-> (show-links BASIC-LINKS-PULL-SIZE) ok)
+      (let [length (-> request :params :length parse-long)]
+        (cond
+          ;; if length = nil -> use default value
+          (nil? length) (-> (show-links BASIC-LINKS-PULL-SIZE) ok)
 
-        ;; if length = 0 -> show all links
-        (zero? length) (-> (show-links) ok)
+          ;; if length = 0 -> show all links
+          (zero? length) (-> (show-links) ok)
 
-        ;; show last <length> links
-        :else (-> (show-links length) ok))))
+          ;; show last <length> links
+          :else (-> (show-links length) ok))))
 
-  (POST "/" request
-    "Creates a new shortened link. Expects JSON body with :url"
+    (POST "/" request
+      "Creates a new shortened link. Expects JSON body with :url"
 
-    (let [body (-> request :params (map->CreateLinkRequest))
-          result (create-link-record! (:url body))]
-      (if (:error result)
-        (bad-request result)
-        (ok result))))
+      (let [body (-> request :params (map->CreateLinkRequest))
+            result (create-link-record! (:url body))]
+        (if (:error result)
+          (bad-request result)
+          (ok result))))
 
-  (DELETE "/:code" request
-    "Delete shortened link by it code. Expects param :code"
+    (DELETE "/:code" request
+      "Delete shortened link by it code. Expects param :code"
 
-    (let [code (-> request :params :code)
-          record (find-link-record code)]
+      (let [code (-> request :params :code)
+            record (find-link-record code)]
 
-      (if record
-        (do (remove-link-record! code)
-            (ok {:message "Link deleted successful"}))
-        (not-found {:message "Link with this code not found"})))))
+        (if record
+          (do (remove-link-record! code)
+              (ok {:message "Link deleted successful"}))
+          (not-found {:message "Link with this code not found"}))))))
--- src/web/routes/service.clj
+++ src/web/routes/service.clj
@@ -1,5 +1,5 @@
 (ns web.routes.service
-  (:require [compojure.core :refer [defroutes GET]]
+  (:require [compojure.core :refer [defroutes context GET]]
             [ring.util.http-response :refer [ok]]
 
             ;; Own code imports
@@ -8,12 +8,13 @@
   (:import (dto.time Uptime)))
 
 (defroutes service-routes
-  (GET "/info" []
-    "Returns server uptime and status information."
+  (context "/api/v1" []
+    (GET "/info" []
+      "Returns server uptime and status information."
 
-    (let [^Uptime uptime (get-server-uptime)]
-      (ok (str "Web server started successfully. Work time: "
-               (:hours uptime) "h "
-               (:minutes uptime) "min "
-               (:seconds uptime) "sec "
-               (:millis uptime) "millis")))))
+      (let [^Uptime uptime (get-server-uptime)]
+        (ok {:message (str "Web server started successfully. Work time: "
+                           (:hours uptime) "h "
+                           (:minutes uptime) "min "
+                           (:seconds uptime) "sec "
+                           (:millis uptime) "millis")})))))
