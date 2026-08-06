083a105875c1252900ec6f279ea2e3a22512fb31
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2026-08-06 23:54:19 +0300

    build(core): import server settings from config.edn

--- src/core.clj
+++ src/core.clj
@@ -1,10 +1,19 @@
 (ns core
-  (:require [ring.adapter.jetty :refer [run-jetty]]
+  (:require [clojure.edn :as edn]
+            [clojure.java.io :as io]
+            [core.time :refer [set-start-time!]]
+            [ring.adapter.jetty :refer [run-jetty]]
             [ring.middleware.json :refer [wrap-json-params wrap-json-response]]
             [ring.middleware.params :refer [wrap-params]]
-            [web.router :refer [app-routes]]
-            [core.time :refer [set-start-time!]]
-            [ring.middleware.params :refer [wrap-params]]))
+            [web.router :refer [app-routes]]))
+
+(def config
+  (delay
+    (if-let [resource (io/resource "config.edn")]
+      (-> resource
+          slurp
+          edn/read-string)
+      (throw (Exception. "config.edn not found in resources")))))
 
 (def app (-> app-routes
              wrap-params
@@ -12,8 +21,11 @@
              (wrap-json-response {:pretty true})))
 
 (defn start-server []
-  (run-jetty app {:port 3000 :join? false})
-  (set-start-time!))
+  (let [port (-> @config :http-port)]
+    (run-jetty app {:port port
+                    :join? false})
+
+    (set-start-time!)))
 
 (defn -main []
   (start-server)
