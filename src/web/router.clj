7259bdd7eca4f4726d1ce2488981855c30872df1
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2026-08-08 02:33:41 +0300

    feat(openapi): created openapi.json route

--- src/web/router.clj
+++ src/web/router.clj
@@ -2,9 +2,11 @@
   (:require [compojure.core :refer [defroutes ANY]]
             [ring.util.http-response :refer [not-found]]
             [web.routes.links :refer [links-routes]]
-            [web.routes.service :refer [service-routes]]))
+            [web.routes.service :refer [service-routes]]
+            [web.routes.openapi :refer [openapi-routes]]))
 
 (defroutes app-routes
-  links-routes
+  openapi-routes
   service-routes
+  links-routes
   (ANY "*" [] (not-found "Not Found")))
